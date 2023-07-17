df6721f9b513278ec48908fdc3bb47c4771c5244
bramadhan <bagaskaramadhan@gmail.com>
2023-07-17 18:40:55 +0700

    GET API books

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,34 @@
 import { Col, Row } from "react-bootstrap";
 import { GenreBooks, ListBooks, NavbarComponent } from "../components";
 import { Component } from "react";
-
+import { API_URL } from "../utils/constant";
+import axios from "axios"
 
 
 export default class Home extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = {
-      list:[]
+      list: []
     }
   }
 
-  componentDidMount(){
-    // TODOss
+  componentDidMount() {
+    axios.get(API_URL + "books")
+      .then((res) => {
+        const list = res.data;
+        this.setState({ list })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
 
   render() {
+    const { list } = this.state
     return (
       <>
         <NavbarComponent />
@@ -28,6 +37,11 @@ export default class Home extends Component {
           <Col>
             <h4>DAFTAR PRODUCT</h4>
           </Col>
+          <Row>
+          {list && list.map((listed) => (
+            <h2>{listed.title}</h2>
+          ))}
+          </Row>
           <GenreBooks />
         </Row>
       </>
